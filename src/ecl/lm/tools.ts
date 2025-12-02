248fdc36770efbfeadb406bee41647fe3f23c780
Gordon Smith <GordonJSmith@gmail.com>
2025-12-02 11:53:43 +0000

    feat: add WUQuery AI tool
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- src/ecl/lm/tools.ts
+++ src/ecl/lm/tools.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import { FindWorkunitsTool } from "./tools/findWorkunits";
 import { FindLogicalFilesTool } from "./tools/findLogicalFiles";
 import { SyntaxCheckTool } from "./tools/syntaxCheck";
 
@@ -7,8 +8,9 @@ let eclLMTools: ECLLMTools;
 export class ECLLMTools {
 
     protected constructor(ctx: vscode.ExtensionContext) {
-        ctx.subscriptions.push(vscode.lm.registerTool("ecl-extension-syntaxCheck", new SyntaxCheckTool()));
+        ctx.subscriptions.push(vscode.lm.registerTool("ecl-extension-findWorkunits", new FindWorkunitsTool()));
         ctx.subscriptions.push(vscode.lm.registerTool("ecl-extension-findLogicalFiles", new FindLogicalFilesTool()));
+        ctx.subscriptions.push(vscode.lm.registerTool("ecl-extension-syntaxCheck", new SyntaxCheckTool()));
     }
 
     static attach(ctx: vscode.ExtensionContext): ECLLMTools {
