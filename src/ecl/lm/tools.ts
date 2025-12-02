154f982a2b99cab4835cf7be5162bc7d669edca2
Gordon Smith <GordonJSmith@gmail.com>
2025-12-02 11:53:43 +0000

    feat: add WU Error/Warning AI tool
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- src/ecl/lm/tools.ts
+++ src/ecl/lm/tools.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import { FindWorkunitsTool } from "./tools/findWorkunits";
+import { GetWorkunitErrorsTool } from "./tools/getWorkunitErrors";
 import { FindLogicalFilesTool } from "./tools/findLogicalFiles";
 import { SyntaxCheckTool } from "./tools/syntaxCheck";
 
@@ -9,6 +10,8 @@ export class ECLLMTools {
 
     protected constructor(ctx: vscode.ExtensionContext) {
         ctx.subscriptions.push(vscode.lm.registerTool("ecl-extension-findWorkunits", new FindWorkunitsTool()));
+        ctx.subscriptions.push(vscode.lm.registerTool("ecl-extension-getWorkunitErrors", new GetWorkunitErrorsTool()));
+
         ctx.subscriptions.push(vscode.lm.registerTool("ecl-extension-findLogicalFiles", new FindLogicalFilesTool()));
         ctx.subscriptions.push(vscode.lm.registerTool("ecl-extension-syntaxCheck", new SyntaxCheckTool()));
     }
